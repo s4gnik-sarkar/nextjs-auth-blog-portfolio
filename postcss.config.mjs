b14d6756ea85ae5999837bc1cf6416cff86b2e87
Sagnik Sarkar <ssagnik825@gmail.com>
2025-10-29 00:04:52 +0530

    fix: removed autoprefixer

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,7 +1,6 @@
-// postcss.config.js
-export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  }
+const config = {
+  plugins: ["@tailwindcss/postcss"],
 };
+
+
+export default config;
